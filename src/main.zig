b05e229bb8f21c43f46f37a0dd69835df78b72f5
Juan Pratama Wahyu Talendra <108502921+juanPWT@users.noreply.github.com>
2024-06-18 11:17:41 +0700

    add game mode

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,14 @@ const std = @import("std");
 const service = @import("service.zig");
 const print = std.debug.print;
 
+var TIME_LIMIT: u16 = 0;
+
+const TimeMode = struct {
+    easy: []const u8 = "easy", // 60000
+    medium: []const u8 = "medium", // 30000
+    hard: []const u8 = "hard", //10000
+};
+
 const Command = struct {
     start: []const u8 = "start",
     help: []const u8 = "help",
@@ -24,17 +32,46 @@ pub fn main() !void {
 
     // condition args
     const cmd = args[1];
+
     const cmdPatern = Command{};
 
     if (std.mem.eql(u8, cmd, cmdPatern.start)) {
+        if (args.len < 3) {
+            print("usage-start: {s} start <mode>", .{args[0]});
+            std.process.exit(1);
+        }
+
+        const cmdStartTime = args[2];
+        const timeMode = TimeMode{};
+        // time mode
+        if (cmdStartTime.len != 0) {
+            if (std.mem.eql(u8, cmdStartTime, timeMode.easy)) {
+                TIME_LIMIT = TIME_LIMIT;
+            } else if (std.mem.eql(u8, cmdStartTime, timeMode.medium)) {
+                TIME_LIMIT = 30000;
+            } else if (std.mem.eql(u8, cmdStartTime, timeMode.hard)) {
+                TIME_LIMIT = 10000;
+            } else {
+                print("mode {s} not exist!", .{cmdStartTime});
+            }
+        }
+
         try start(cmdPatern);
     } else if (std.mem.eql(u8, cmd, cmdPatern.help)) {
         print(
             \\ +---------------------------------------------+
             \\ |   list command gomu-gomu:                   |
             \\ |                                             |
-            \\ |   start          for start a game           |
+            \\ |   start <mode>   for start a game           |
             \\ |   help           here                       |
+            \\ |                                             |
+            \\ |   game mode:                                |
+            \\ |                                             |
+            \\ |   usage: gomu-gomu start <mode>             |
+            \\ |   mode :                                    |
+            \\ |   easy         [60s],                       |
+            \\ |   medium       [30s],                       |
+            \\ |   hard         [10s],                       |
             \\ |                                             |
             \\ +---------------------------------------------+
         , .{});
@@ -49,11 +86,25 @@ fn start(cmdPatern: Command) !void {
     const stdin = std.io.getStdIn().reader();
     var line: [256]u8 = undefined;
 
-    // TODO: randword execute again when input == randWord
-    // else randword not execute until input == randWord;
     var randWord: []const u8 = service.generateWord();
+    var wordTotal: u8 = 0;
+
+    const startTime = std.time.milliTimestamp();
 
     while (true) {
+        // init time
+        const currentTime = std.time.milliTimestamp();
+        const elapsedTime = currentTime - startTime;
+        if (elapsedTime >= TIME_LIMIT) {
+            print(
+                \\   +----------------------------------------------+
+                \\          time       :  {}ms                   
+                \\          word total :  {}                        
+                \\   +______________________________________________+
+            , .{ TIME_LIMIT, wordTotal });
+            break;
+        }
+
         print("run quit for exit!\n", .{});
         print("try : {s}\n", .{randWord});
         print("your: ", .{});
@@ -72,6 +123,7 @@ fn start(cmdPatern: Command) !void {
 
         if (std.mem.eql(u8, randWord, input)) {
             print("YES\n", .{});
+            wordTotal += 1;
             randWord = service.generateWord();
         } else {
             print("!WRONG\n", .{});
